53b5e02d4fe426f5c9363f3fcd4035060e9dcb31
ionutforyou <ionutforyou@gmail.com>
2015-07-09 14:38:02 +0300

    Changes in the date / time backup dir
    The time is appended to the backup file automatically, no matter the options, only the date is either appended to the backup file or as a folder, depending on the backup_per_time setting (file,folder, false). like:
    pathoffile/D-A-T-E/file_time.ext    -> when backup_per_time is "folder"
    pathoffile/file_D-A-T-E_time.ext    -> when backup_per_time is "file"
    pathoffile/file_time.ext  -> when backup_per_time is "false"

--- autobackups/paths_helper.py
+++ autobackups/paths_helper.py
@@ -1,7 +1,9 @@
-# @author 		Avtandil Kikabidze
-# @copyright 		Copyright (c) 2008-2014, Avtandil Kikabidze aka LONGMAN (akalongman@gmail.com)
-# @link 			http://long.ge
-# @license 		GNU General Public License version 2 or later;
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author ........Avtandil Kikabidze
+# @copyright ........Copyright (c) 2008-2014, Avtandil Kikabidze aka LONGMAN (akalongman@gmail.com)
+# @link ............http://long.ge
+# @license ........GNU General Public License version 2 or later; 
 
 import sublime
 import os
@@ -9,101 +11,112 @@ import re
 import sys
 import datetime
 
+
 class PathsHelper(object):
-	platform = False
-	backup_dir = False
-	backup_per_day = False
-	backup_per_time = False
 
-	@staticmethod
-	def initialize(pl, backup_dir, backup_per_day, backup_per_time):
-		PathsHelper.platform = pl
-		PathsHelper.backup_dir = backup_dir
-		PathsHelper.backup_per_day = backup_per_day
-		PathsHelper.backup_per_time = backup_per_time
+    platform = False
+    backup_dir = False
+    backup_per_day = False
+    backup_per_time = False
 
+    @staticmethod
+    def initialize(
+        pl,
+        backup_dir,
+        backup_per_day,
+        backup_per_time,
+        ):
+        PathsHelper.platform = pl
+        PathsHelper.backup_dir = backup_dir
+        PathsHelper.backup_per_day = backup_per_day
+        PathsHelper.backup_per_time = backup_per_time
 
-	@staticmethod
-	def get_base_dir(only_base):
-		# Configured setting
-		backup_dir = PathsHelper.backup_dir
-		now_date = str(datetime.datetime.now())
-		date = now_date[:10]
+    @staticmethod
+    def get_base_dir(only_base):
 
-		backup_per_day = PathsHelper.backup_per_day
-		if (backup_per_day and not only_base):
-			backup_dir = backup_dir +'/'+ date
+        # Configured setting
 
-		time = now_date[11:19].replace(':', '')
-		backup_per_time = PathsHelper.backup_per_time
-		if (backup_per_day and backup_per_time == 'folder' and not only_base):
-			backup_dir = backup_dir +'/'+ time
+        backup_dir = PathsHelper.backup_dir
+        now_date = str(datetime.datetime.now())
+        date = now_date[:10]
 
-		if backup_dir != '':
-			return os.path.expanduser(backup_dir)
+        backup_per_day = PathsHelper.backup_per_day
+        backup_per_time = PathsHelper.backup_per_time
 
-		# Windows: <user folder>/My Documents/Sublime Text Backups
-		if (PathsHelper.platform == 'Windows'):
-			backup_dir = 'D:/Sublime Text Backups'
-			if (backup_per_day and not only_base):
-				backup_dir = backup_dir +'/'+ date
-			return backup_dir
+        if backup_dir != '':
+            return os.path.expanduser(backup_dir)
 
-		# Linux/OSX/other: ~/sublime_backups
-		backup_dir = '~/.sublime/backups'
-		if (backup_per_day and not only_base):
-			backup_dir = backup_dir +'/'+ date
-		return os.path.expanduser(backup_dir)
+        # Windows: <user folder>/My Documents/Sublime Text Backups
 
-	@staticmethod
-	def timestamp_file(filename):
-		(filepart, extensionpart) = os.path.splitext(filename)
+        if PathsHelper.platform == 'Windows':
+            backup_dir = 'D:/Sublime Text Backups'
+            return backup_dir
 
-		backup_per_day =  PathsHelper.backup_per_day
-		backup_per_time =  PathsHelper.backup_per_time
-		if (backup_per_day and backup_per_time == 'file'):
-			now_date = str(datetime.datetime.now())
-			time = now_date[11:19].replace(':', '')
-			name = '%s_%s%s' % (filepart, time, extensionpart,)
-		else:
-			name = '%s%s' % (filepart, extensionpart,)
-		return name
+        # Linux/OSX/other: ~/sublime_backups
 
-	@staticmethod
-	def get_backup_path(filepath):
-		path = os.path.expanduser(os.path.split(filepath)[0])
-		backup_base = PathsHelper.get_base_dir(False)
-		path = PathsHelper.normalise_path(path)
-		return os.path.join(backup_base, path)
+        backup_dir = '~/.sublime/backups'
+        return os.path.expanduser(backup_dir)
 
-	@staticmethod
-	def normalise_path(path, slashes = False):
-		if (path is None):
-			return ''
+    @staticmethod
+    def timestamp_file(filename):
+        (filepart, extensionpart) = os.path.splitext(filename)
+        name = filename
+        backup_per_day = PathsHelper.backup_per_day
+        backup_per_time = PathsHelper.backup_per_time
+        now_date = str(datetime.datetime.now())
+        date = now_date[:10]
+        time = now_date[11:19].replace(':', '-')
+        if backup_per_day:
+            if backup_per_time == 'file':
+                name = '%s_%s_%s%s' % (filepart, date, time,
+                        extensionpart)
+            if backup_per_time == 'folder':
+                name = '%s/%s_%s%s' % (date, filepart, time,
+                        extensionpart)
+        else:
+            if backup_per_time == 'file':
+                name = '%s_%s%s' % (filepart, time, extensionpart)
+            if backup_per_time == 'folder':
+                name = '%s/%s%s' % (time, filepart, extensionpart)
 
-		if PathsHelper.platform != 'Windows':
-			# remove any leading / before combining with backup_base
-			path = re.sub(r'^/', '', path)
-			return path
+        return name
 
-		path = path.replace('/', '\\')
+    @staticmethod
+    def get_backup_path(filepath):
+        path = os.path.expanduser(os.path.split(filepath)[0])
+        backup_base = PathsHelper.get_base_dir(False)
+        path = PathsHelper.normalise_path(path)
+        return os.path.join(backup_base, path)
 
+    @staticmethod
+    def normalise_path(path, slashes=False):
+        if path is None:
+            return ''
 
-		# windows only: transform C: into just C
-		path = re.sub(r'^(\w):', r'\1', path)
+        if PathsHelper.platform != 'Windows':
 
-		# windows only: transform \\remotebox\share into network\remotebox\share
-		path = re.sub(r'^\\\\([\w\-]{2,})', r'network\\\1', path)
+            # remove any leading / before combining with backup_base
 
-		if slashes:
-			path = path.replace('\\', '/')
+            path = re.sub(r'^/', '', path)
+            return path
 
-		return path
+        path = path.replace('/', '\\')
 
+        # windows only: transform C: into just C
 
+        path = re.sub(r'^(\w):', r'\1', path)
 
-	@staticmethod
-	def get_backup_filepath(filepath):
-		filename = os.path.split(filepath)[1]
-		return os.path.join(PathsHelper.get_backup_path(filepath), PathsHelper.timestamp_file(filename))
+        # windows only: transform \\remotebox\share into network\remotebox\share
 
+        path = re.sub(r'^\\\\([\w\-]{2,})', r'network\\\1', path)
+
+        if slashes:
+            path = path.replace('\\', '/')
+
+        return path
+
+    @staticmethod
+    def get_backup_filepath(filepath):
+        filename = os.path.split(filepath)[1]
+        return os.path.join(PathsHelper.get_backup_path(filepath),
+                            PathsHelper.timestamp_file(filename))
